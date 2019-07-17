07ea625fd5c86d3ec851035a09eb8eaf7a9885a6
Olivia Kaufmann <olivia.kaufmann@ilub.unibe.ch>
2019-07-17 09:28:43 +0200

    fix for 1162

--- js/ilEtherpadLite.js
+++ js/ilEtherpadLite.js
@@ -20,7 +20,7 @@
             height = $(window).height();
         }
         else {
-            height = $(window).height() - ($("div.ilMainHeader").height() + $("div.ilTopBar").height() + $("footer.ilFooter").height() + $("div.il_HeaderInner").height() + 300);
+            height = $(window).height() - ($("div.ilMainHeader").height() + $("div.ilTopBar").height() + $("footer.ilFooter").height() + $("div.il_HeaderInner").height() + 200);
         }
         repaintPad();
     }
